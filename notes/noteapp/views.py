0cd70fa64a27c8a8ea12883f1d2115487fe289e8
II-777 <45958058+II-777@users.noreply.github.com>
2023-12-14 20:47:09 -0500

    connect users and notes

--- notes/noteapp/views.py
+++ notes/noteapp/views.py
@@ -1,25 +1,33 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import TagForm
-from .forms import NoteForm
-from .models import Tag
-from .models import Note
+from .forms import TagForm, NoteForm
+from .models import Tag, Note
+
 
 # Create your views here.
+@login_required
 def main(request):
-    notes = Note.objects.all()
+    notes = (
+        Note.objects.filter(user=request.user).all()
+        if request.user.is_authenticated
+        else []
+    )
     return render(request, "noteapp/index.html", {"notes": notes})
 
 
+@login_required
 def set_done(request, note_id):
-    Note.objects.filter(pk=note_id).update(done=True)
+    Note.objects.filter(pk=note_id, user=request.user).update(done=True)
     return redirect(to="noteapp:main")
 
 
+@login_required
 def delete_note(request, note_id):
-    Note.objects.get(pk=note_id).delete()
+    Note.objects.get(pk=note_id, user=request.user).delete()
     return redirect(to="noteapp:main")
 
 
+@login_required
 def tag(request):
     if request.method == "POST":
         form = TagForm(request.POST)
@@ -32,15 +40,19 @@ def tag(request):
     return render(request, "noteapp/tag.html", {"form": TagForm()})
 
 
+@login_required
 def note(request):
-    tags = Tag.objects.all()
+    tags = Tag.objects.filter(user=request.user).all()
 
     if request.method == "POST":
         form = NoteForm(request.POST)
         if form.is_valid():
-            new_note = form.save()
-
-            choice_tags = Tag.objects.filter(name__in=request.POST.getlist("tags"))
+            new_note = form.save(commit=False)
+            new_note.user = request.user
+            new_note.save()
+            choice_tags = Tag.objects.filter(
+                name__in=request.POST.getlist("tags"), user=request.user
+            )
             for tag in choice_tags.iterator():
                 new_note.tags.add(tag)
 
@@ -51,6 +63,12 @@ def note(request):
     return render(request, "noteapp/note.html", {"tags": tags, "form": NoteForm()})
 
 
+@login_required
 def detail(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
     return render(request, "noteapp/detail.html", {"note": note})
+
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
